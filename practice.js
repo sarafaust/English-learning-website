3d1a0a0f01263f700cf9d1677077bc215e32cd9f
sarafaust <sarafaust1995@gmail.com>
2021-01-22 01:55:20 +0200

    exercise work well

--- practice.js
+++ practice.js
@@ -7,9 +7,13 @@ var inCorrArr = [];
 var CorrArr;
 var g_points = 0
 var g_points_arr = [4,3,2,1]
+var g_points_Helparr = [3,2,2,1]
+var wordlist_tmp
+
 function updateDataPage()
 {
 
+	// document.getElementById("imageWID").style.background  = "#f3f3f3 url('img_tree.png') no-repeat right top";;
 	CorrArr = Array(exerciseWordList.length).fill(0);
 	html_code = "";
 	for (i = 0; i < wordsList.length; i++) {
@@ -75,3 +79,58 @@ function calcPoints(numIncorr)
     return g_points_arr[3]
 	}
 }
+
+function showAns()
+{
+	var answer = window.confirm("שים לב, ציונך לשיעור זה יופחת! להמשיך? ");
+	if (answer) {
+	    //some code
+	wordnum = 0;
+	for(i=0;i<exerciseAnsWordList.length;i++)
+	{
+		if(exerciseAnsWordList[g_currentWord] == wordsList[i])
+		{
+			wordnum = i;
+			break;
+		}
+	}
+	id = "wordID"+wordnum;
+	document.getElementById(id).style.backgroundColor = "green";
+	for(i=0; i<inCorrArr.length;i++)
+		document.getElementById(inCorrArr[i]).style.backgroundColor = "white";
+
+	CorrArr[wordnum] = 1
+	document.getElementById("wrappWordID"+g_currentWord).className = "listWordsC";
+	document.getElementById("ansWordID"+g_currentWord).innerHTML  = exerciseAnsWordList[g_currentWord]
+
+	g_currentWord++
+	document.getElementById("wrappWordID"+(g_currentWord)).className = "listWordsCSelect";
+
+	//points
+	g_points_arr = g_points_Helparr
+
+	inCorrArr = []
+  }
+}
+
+function vacableFun()
+{
+	window.open("vocabulary_page.html", "_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=200,width=600,height=800");
+}
+
+function InsBtnFun()
+{
+	window.open("instruction_lesson.html", "_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=200,width=600,height=800");
+}
+
+function anotherExplnationBtnFunc()
+{
+	window.open("help.pdf", "_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=200,width=600,height=800");
+}
+
+function grammerSignsBtnFun()
+{
+
+	window.open("grammerSign_page.html", "_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=200,width=600,height=800");
+}
+
