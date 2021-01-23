4034a07983ea733bdda42bbb7ac880442a3b73ea
hodayadh <sarafaust1995@gmail.com>
2021-01-24 01:57:47 +0200

    Dict js work well;

---
+++ dictionary.js
@@ -0,0 +1,18 @@
+inputDictArray =["a"]
+inputDictOtherFormatArray =["an"]
+hebDictArr = ["תנועה, מופיעה באנגלית לפני שם עצם (אחד) לא מיודע. "]
+var GLOBAL_DICT = {};
+
+function createDict()
+{
+	for (var i = 0; i < inputDictArray.length; i++) {
+		GLOBAL_DICT[inputDictArray[i]] = hebDictArr[i];
+		GLOBAL_DICT[inputDictOtherFormatArray[i]] = hebDictArr[i];
+	}
+}
+
+function translateWord()
+{
+	inputText = document.getElementById("dictInTextID").value;
+	document.getElementById("dictTranID").innerHTML = GLOBAL_DICT[inputText]
+}
