70424df32f7f7b8c28d66d9dcd1c6613180144c9
sarafaust <sarafaust1995@gmail.com>
2021-03-18 23:03:33 +0200

    get permmision option

---
+++ buyPermission.js
@@ -0,0 +1,23 @@
+//include englishWeb.js
+var imported = document.createElement('script');
+imported.src = 'englishWeb.js';
+document.head.appendChild(imported);
+
+GLOBAL_PERMISSION = "FREE2021"
+function checkCodePerm()
+{
+    var persmission = document.getElementById("btnPerID").nodeValue
+    if(persmission == GLOBAL_PERMISSION)
+    {
+        userKey = getUserKey()
+            $.ajax({
+            type: "POST",
+            url: "buyServer.php",
+            async: false,
+            data: {userKey: userKey},
+            success: function(response) { resualt = response; }
+            });
+
+        return resualt
+    }
+}
